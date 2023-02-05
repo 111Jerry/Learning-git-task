87822cfa036a7b721696c7d86de566fc7a4af84f
Jerzy Ziulkowski <jerry.111@o2.pl>
2023-02-05 21:29:57 +0100

    Add new type of shop to the list

---
+++ List_shop_dict.py
@@ -0,0 +1,16 @@
+buying_of_prod = {
+    "piekarni": ["chleb", "bułki", "pączki"],
+    "warzywniaka": ["marchew", "seler", "rukola"]
+}
+
+print("Lista zakupów", "\n")
+
+for (shop, items) in buying_of_prod.items():
+    items = ",".join(items)
+    items = items.title()
+    print(f"Idę do {shop.capitalize()}", "i kupuję tam:", items)
+
+all_products = 0
+for values in buying_of_prod.values():
+    all_products += len(values)
+print("W sumie kupuję:{} produktów".format(all_products))
